972289d0c351611634171434542670dd437fe601
Hugh Rawlinson <hughr2005@gmail.com>
2020-03-23 22:11:35 -0400

    Add hook to get COVID-19 data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+function useCovidData() {
+  const COVID_DATA_URL = "https://pomber.github.io/covid19/timeseries.json";
+  const [covidData, setCovidData] = useState(null);
+
+  useEffect(() => {
+    let isCurrent = true;
+    fetch(COVID_DATA_URL).then(res => res.json()).then(data => {
+      if (isCurrent) setCovidData(data);
+    });
+    return () => {
+      isCurrent = false;
+    };
+  }, []);
+
+  return covidData
+}
 
 function App() {
+  const covidData = useCovidData();
+
+  console.log(covidData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
