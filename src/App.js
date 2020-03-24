4b76e966cfbc7986246add31245399530e5c5505
Hugh Rawlinson <hughr2005@gmail.com>
2020-03-23 22:30:50 -0400

    Extract useCovidData to file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import React, {useState, useEffect} from 'react';
-
-function useCovidData() {
-  const COVID_DATA_URL = "https://pomber.github.io/covid19/timeseries.json";
-  const [covidData, setCovidData] = useState(null);
-
-  useEffect(() => {
-    let isCurrent = true;
-    fetch(COVID_DATA_URL).then(res => res.json()).then(data => {
-      if (isCurrent) setCovidData(data);
-    });
-    return () => {
-      isCurrent = false;
-    };
-  }, []);
-
-  return covidData
-}
+import React from 'react';
+import { useCovidData } from './hooks/useCovidData';
 
 function App() {
   const covidData = useCovidData();
---
+++ src/hooks/useCovidData.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export function useCovidData() {
+  const COVID_DATA_URL = "https://pomber.github.io/covid19/timeseries.json";
+  const [covidData, setCovidData] = useState(null);
+
+  useEffect(() => {
+    let isCurrent = true;
+    fetch(COVID_DATA_URL).then(res => res.json()).then(data => {
+      if (isCurrent) setCovidData(data);
+    });
+    return () => {
+      isCurrent = false;
+    };
+  }, []);
+
+  return covidData
+}
+
