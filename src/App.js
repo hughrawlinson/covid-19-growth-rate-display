30aeac7f92e50213eb144b647d3a10dd32084aea
Hugh Rawlinson <hughr2005@gmail.com>
2020-03-23 22:39:02 -0400

    Show demo line graph

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { useCovidData } from './hooks/useCovidData';
-
+import { Line } from '@nivo/line';
 function App() {
   const covidData = useCovidData();
+  const lineConfig = {
+    width: 900,
+    height: 400,
+    margin: { top: 20, right: 20, bottom: 60, left: 80},
+    animate: true,
+    enableSlices: 'x'
+  }
 
   console.log(covidData);
 
   return (
     <div className="App">
+      { covidData && (
+        <Line
+          curve="monotoneX"
+          data={[{
+            id: "US",
+            data: covidData.US.map(({confirmed}, index) => ({
+              x: index,
+              y: confirmed
+            }))
+          }]}
+          {...lineConfig}
+        />
+      )}
     </div>
   );
 }
