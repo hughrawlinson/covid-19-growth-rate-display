58e274531d265106680682eaff79a72b0c843afe
Hugh Rawlinson <hughr2005@gmail.com>
2020-03-23 23:56:59 -0400

    Add Country selector

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useCovidData } from './hooks/useCovidData';
 import { Line } from '@nivo/line';
+import Select from 'react-select';
 
 function App() {
   const [covidData, covidCountries] = useCovidData();
-  console.log(covidCountries);
+  const [selectedCountry, setSelectedCountry] = useState('Afghanistan');
+
+  useEffect(() => {
+    if (covidCountries && !selectedCountry) setSelectedCountry(covidCountries[0]);
+  }, [selectedCountry, covidCountries]);
 
   const lineConfig = {
     width: 900,
     height: 400,
     margin: { top: 20, right: 20, bottom: 60, left: 80},
     animate: true,
-    enableSlices: 'x'
+    enableSlices: 'x',
   }
 
   return (
     <div className="App">
       { covidData && (
+        <>
         <Line
           curve="monotoneX"
           data={[{
-            id: "US",
-            data: covidData.US.map(({confirmed}, index) => ({
+            id: selectedCountry,
+            data: covidData[selectedCountry].map(({confirmed}, index) => ({
               x: index,
               y: confirmed
             }))
           }]}
           {...lineConfig}
         />
+        <Select
+          options={covidCountries.map(country => ({value: country, label: country}))}
+          onChange={value => {
+            setSelectedCountry(value.value)
+          }}
+          value={{
+            value: selectedCountry,
+            label: selectedCountry,
+          }}
+        />
+        </>
       )}
     </div>
   );
